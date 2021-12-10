eea6a5f8ecdf4f23500034d5d86ceed683652e40
skajmera <subhashajmera2@gmail.com>
2021-12-10 13:48:23 +0530

    jwt

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 require("./mongo");
 const PORT = process.env.PORT || 5000;
 fastify.register(require("./user/user.routes"));
+
 const startServer = async () => {
   try {
     await fastify.listen(PORT);
@@ -12,22 +13,15 @@ const startServer = async () => {
   }
 };
 startServer();
-
 /////////////////////////////
-// require('./mongo')
-// const fastify = require('fastify')({ logger: true })
-// fastify.register(require('./user/user.routes'));
+// const fastify = require("fastify")
+// require("dotenv").config();
+// require("./mongo");
+// const app=fastify()
+// const PORT = process.env.PORT || 5000;
+// const userRoutes=require("./user/user.routes")
+// app.register(userRoutes,{prefix:'/user'})
 
-// fastify.get('/',function (req, reply) {
-//   console.log(req.body);
-//   reply.send({ hello: 'world!'})
-// })
-
-// fastify.post('/users',function (req, reply) {
-//     console.log(req.body);
-//     reply.send({ hello: 'world!' })
-//   })
-
-// fastify.listen(3000,()=>{
-//     console.log("connected...");
+// app.listen(PORT,()=>{
+//   console.log('coonected..',PORT);
 // })
