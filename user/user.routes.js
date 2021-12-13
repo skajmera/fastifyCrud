91190a8ea4cfef30286bdbaca7c045600d3e50eb
skajmera <subhashajmera2@gmail.com>
2021-12-13 18:34:36 +0530

    successfull file uploaded

--- user/user.routes.js
+++ user/user.routes.js
@@ -1,21 +1,25 @@
 const userController = require("../user/user.controller");
-const {authenticateToken}=require('../utils/jwt');
-const {myfunction}=require('../utils/nodemailer')
-const multer = require('fastify-multer') 
-const upload = multer({ dest: 'public/uploads/' })
+const { authenticateToken } = require("../utils/jwt");
+const { myfunction } = require("../utils/nodemailer");
+const multer = require("fastify-multer");
+const upload = multer({ dest: "public/uploads/" });
 
 async function routes(fastify, options) {
-  fastify.get('/sendmail/:email', (req, reply, next) => {
-    myfunction(fastify,reply,req)
-  })
-  fastify.register(multer.contentParser)
-  fastify.post("/profile",{preHandler:upload.single('avatar')},async(req, res) => {
-    return res.send({ hello: "seccessfull uploaded picture" });
+  fastify.get("/sendmail/:email", (req, reply, next) => {
+    myfunction(fastify, reply, req);
   });
 
+  fastify.register(multer.contentParser);
+  fastify.post(
+    "/profile",
+    { preHandler: upload.single("avatar") },
+    async (req, res) => {
+      return res.send({ hello: "seccessfull uploaded picture" });
+    }
+  );
 
-  fastify.get("/", {preHandler:authenticateToken},async (req, res) => {
-    return ({ hello: "subhash" });
+  fastify.get("/", { preHandler: authenticateToken }, async (req, res) => {
+    return { hello: "subhash" };
   });
 
   fastify.post("/insertData", async (req, res) => {
@@ -28,15 +32,23 @@ async function routes(fastify, options) {
     return response.send(result);
   });
 
-  fastify.put("/updateById",  {preHandler:authenticateToken},async (req, res) => {
-    const result = await userController.updateUser(req);
-    return res.send(result);
-  });
+  fastify.put(
+    "/updateById",
+    { preHandler: authenticateToken },
+    async (req, res) => {
+      const result = await userController.updateUser(req);
+      return res.send(result);
+    }
+  );
 
-  fastify.delete("/deleteById", {preHandler:authenticateToken}, async (req, res) => {
-    const result = await userController.deleteUser(req);
-    return res.send(result);
-  });
+  fastify.delete(
+    "/deleteById",
+    { preHandler: authenticateToken },
+    async (req, res) => {
+      const result = await userController.deleteUser(req);
+      return res.send(result);
+    }
+  );
 }
 
 module.exports = routes;
